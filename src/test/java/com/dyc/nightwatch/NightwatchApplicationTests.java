1481f237c5016506b70c26647cfd1de99606eec6
dingmiao <1486201358@qq.com>
2022-03-05 20:19:37 +0800

    init

---
+++ src/test/java/com/dyc/nightwatch/NightwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyc.nightwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightwatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
